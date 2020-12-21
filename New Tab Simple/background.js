8aa9777f24eb780642bc5c94257d7bca96e1f0a6
ArcaEge <40526225+ArcaEge@users.noreply.github.com>
2020-12-21 23:06:29 +0000

    Fixed Manifest for manifest v3

--- New Tab Simple/background.js
+++ New Tab Simple/background.js
@@ -1,8 +1,6 @@
 chrome.runtime.onInstalled.addListener(function(details){
-  if (details.reason == "install"){
     chrome.storage.local.set({'img_type': 'nature'})
     chrome.storage.local.set({'quotes': true})
     chrome.storage.local.set({'changeQuote': 2})
     chrome.storage.sync.set({'sync': true})
-  }
 });
