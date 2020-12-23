834d36f37d135c1ca67c47307055e9709f30b10c
ArcaEge <40526225+ArcaEge@users.noreply.github.com>
2020-12-23 22:10:57 +0000

    Fixed bugs - Bugs left to fix: When image is changed quotes are empty and need a refresh

--- New Tab Simple/bg.js
+++ New Tab Simple/bg.js
@@ -3,16 +3,16 @@ var dt;
 
 // Loader
 
-$('body').append('<div style="" id="loadingDiv"><div class="loader"></div></div>');
-$(window).on('load', function(){
-  setTimeout(removeLoader, 0);
-});
-
-function removeLoader(){
-    $("#loadingDiv").fadeOut(500, function() {
-      $("#loadingDiv").remove();
-  });
-}
+// $('body').append('<div style="" id="loadingDiv"><div class="loader"></div></div>');
+// $(function(){
+//   setTimeout(removeLoader, 0);
+// });
+//
+// function removeLoader(){
+//     $("#loadingDiv").fadeOut(250, function() {
+//       $("#loadingDiv").remove();
+//   });
+// }
 
 /* --------Background-------- */
 // Set Background
@@ -74,7 +74,7 @@ function get_set_sync(data) {
 
 // Get Background
 
-$(document).ready(function() {
+// $(document).ready(function() {
   dt = new Date();
   chrome.storage.sync.get(['sync'], function (data) {
     if (data.sync) {
@@ -83,32 +83,46 @@ $(document).ready(function() {
       chrome.storage.local.get(['url_img', 'description', 'download_url', 'html_url', 'user', 'username', 'expires_date', 'expires_month', 'expires_year', 'quotes'], checkStuff)
     }
   })
-});
+// });
 
 function checkStuff(data) {
-  var body = document.querySelector("body");
+  var body;
+  $(document).ready(function() {
+    body = document.querySelector("body");
+  });
   dt = new Date();
   if (data.expires_date != dt.getDate() || data.expires_month != dt.getMonth() || data.expires_year != dt.getFullYear()) {
     get_set()
     if (data.quotes){
-      body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.04), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7)), url('" + data.url_img + "')";
+      $(document).ready(function() {
+        body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.04), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7)), url('" + data.url_img + "')";
+      });
+      main();
+      console.log("hiii")
     } else {
-      body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.1)), url('" + data.url_img + "')";
+      $(document).ready(function() {
+        body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.1)), url('" + data.url_img + "')";
+      });
     }
     if (data.description != null){
-      $('#photo').tooltip({title: data.description, animation: true});
+      $(document).ready(function() {
+        $('#photo').tooltip({title: data.description, animation: true});
+      });
     }
   } else{
-    document.querySelector('#attribution').innerHTML = "<a class='text-light' id='photo' href='" + data.html_url + "'>Photo</a> by <a href='" + data.user + "' class='text-light'>" + data.username + "</a> on <a href='https://unsplash.com' class='text-light'>Unsplash</a>";
-    if (data.description != null){
-      $('#photo').tooltip({title: data.description, animation: true});
-    }
-    if (data.quotes){
-      body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.04), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7)), url('" + data.url_img + "')";
-    } else {
-      body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.1)), url('" + data.url_img + "')";
-    }
-    body.style.height = document.documentElement.clientHeight + "px"
+    $(document).ready(function() {
+      var body = document.querySelector("body");
+      document.querySelector('#attribution').innerHTML = "<a class='text-light' id='photo' href='" + data.html_url + "'>Photo</a> by <a href='" + data.user + "' class='text-light'>" + data.username + "</a> on <a href='https://unsplash.com' class='text-light'>Unsplash</a>";
+      if (data.description != null){
+        $('#photo').tooltip({title: data.description, animation: true});
+      }
+      if (data.quotes){
+        body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.04), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7)), url('" + data.url_img + "')";
+      } else {
+        body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.1)), url('" + data.url_img + "')";
+      }
+      body.style.height = document.documentElement.clientHeight + "px"
+    });
   }
 }
 
@@ -157,3 +171,135 @@ const q = $('#query');
       console.log(q.value)
       chrome.search.query({text: q.val()});
   });
+
+// Quotes
+
+function show_quote(qt, thr){
+  document.getElementById('quote').innerHTML = qt
+  if (thr != undefined || thr != null) {
+    // document.getElementById('author-footer').innerHTML = '<footer class="blockquote-footer" id="author" style="color: #ababab"></footer>'
+    document.getElementById('author').innerHTML = thr
+  } else {
+    document.getElementById('author').innerHTML = "Unknown"
+  }
+}
+
+function main_local() {
+  chrome.storage.local.get(['quotes'], function(data){
+    if (data.quotes){
+      chrome.storage.local.get(['quote', 'author', 'changeQuote', 'expires_date', 'expires_month', 'expires_year'], function(data){
+        if (data.changeQuote == 2) {
+          $(document).ready(function() {
+            var dt = new Date();
+            if (data.quote != undefined && data.author != undefined) {
+              if (data.expires_date == dt.getDate() && data.expires_month == dt.getMonth() && data.expires_year == dt.getFullYear()) {
+                show_quote(data.quote, data.author)
+              }
+            } else {
+              fetch("https://type.fit/api/quotes")
+              .then(response => response.json())
+              .then(data => {
+                // console.log(data);
+                var quoteId = Math.floor(Math.random() * data.length)
+                // console.log(data[quoteId].author)
+                // show_quote(data[quoteId].text, data[quoteId].author)
+                var quote = data[quoteId].text;
+                var author = data[quoteId].author;
+                var to_return = [quote, author]
+                // console.log(to_return)
+                var qte = to_return;
+                // console.log(qte)
+                chrome.storage.local.set({'quote': qte[0]})
+                chrome.storage.local.set({'author': qte[1]})
+                show_quote(qte[0], qte[1])
+            });
+            }
+          })
+        } else {
+            fetch("https://type.fit/api/quotes")
+            .then(response => response.json())
+            .then(data => {
+              // console.log(data);
+              var quoteId = Math.floor(Math.random() * data.length)
+              // console.log(data[quoteId].author)
+              // show_quote(data[quoteId].text, data[quoteId].author)
+              var quote = data[quoteId].text;
+              var author = data[quoteId].author;
+              var to_return = [quote, author]
+              // console.log(to_return)
+              var qte1 = to_return;
+              show_quote(qte1[0], qte1[1])
+            });
+          }
+      })
+    } else {
+      document.querySelector('.quote').remove()
+    }
+  })
+}
+
+function main_sync() {
+  chrome.storage.sync.get(['quotes'], function(data){
+    if (data.quotes){
+      chrome.storage.sync.get(['quote', 'author', 'changeQuote', 'expires_date', 'expires_month', 'expires_year'], function(data){
+        if (data.changeQuote == 2) {
+          $(document).ready(function() {
+          var dt = new Date();
+            if (data.quote != undefined && data.author != undefined) {
+              if (data.expires_date == dt.getDate() && data.expires_month == dt.getMonth() && data.expires_year == dt.getFullYear()) {
+                show_quote(data.quote, data.author)
+              }
+            } else {
+              fetch("https://type.fit/api/quotes")
+              .then(response => response.json())
+              .then(data => {
+                // console.log(data);
+                var quoteId = Math.floor(Math.random() * data.length)
+                // console.log(data[quoteId].author)
+                // show_quote(data[quoteId].text, data[quoteId].author)
+                var quote = data[quoteId].text;
+                var author = data[quoteId].author;
+                var to_return = [quote, author]
+                // console.log(to_return)
+                var qte = to_return;
+                // console.log(qte)
+                chrome.storage.sync.set({'quote': qte[0]})
+                chrome.storage.sync.set({'author': qte[1]})
+                show_quote(qte[0], qte[1])
+            });
+            }
+          })
+        } else {
+            fetch("https://type.fit/api/quotes")
+            .then(response => response.json())
+            .then(data => {
+              // console.log(data);
+              var quoteId = Math.floor(Math.random() * data.length)
+              // console.log(data[quoteId].author)
+              // show_quote(data[quoteId].text, data[quoteId].author)
+              var quote = data[quoteId].text;
+              var author = data[quoteId].author;
+              var to_return = [quote, author]
+              // console.log(to_return)
+              var qte1 = to_return;
+              show_quote(qte1[0], qte1[1])
+            });
+          }
+      })
+    } else {
+      document.querySelector('.quote').remove()
+    }
+  })
+}
+
+function main() {
+  chrome.storage.sync.get(['sync'], function(data){
+    if(data.sync) {
+      main_sync()
+    } else {
+      main_local()
+    }
+  });
+}
+
+main()
--- New Tab Simple/options.js
+++ New Tab Simple/options.js
@@ -15,58 +15,106 @@ function close_discard() {
 }
 
 document.getElementById("alert").style.display = "none";
-function save_options() {
 
+function save_options() {
   var type_img = document.getElementById('image_type').value;
-  chrome.storage.local.get(['img_type'], function (data) {
-    if (type_img != data.img_type) {
-      chrome.storage.local.remove(['expires_date', 'expires_month', 'expires_year'])
+  if (document.getElementById('syncCheckInner').checked) {
+    chrome.storage.sync.get(['img_type'], function (data) {
+      if (type_img != data.img_type) {
+        chrome.storage.sync.remove(['expires_date', 'expires_month', 'expires_year'])
+      }
+    })
+    if (document.getElementById('newtab').checked) {
+      var quote_change = 1
+    } else if (document.getElementById('day').checked) {
+      var quote_change = 2
     }
-  })
-  if (document.getElementById('newtab').checked) {
-    var quote_change = 1
-  } else if (document.getElementById('day').checked) {
-    var quote_change = 2
+
+    var quotes_checked = document.getElementById('quoteSwitch').checked;
+    chrome.storage.sync.set({'sync': document.getElementById('syncCheckInner').checked});
+    chrome.storage.sync.set({'img_type': type_img});
+    chrome.storage.sync.set({'quotes': quotes_checked});
+    chrome.storage.sync.set({'changeQuote': quote_change});
+  } else {
+    chrome.storage.local.get(['img_type'], function (data) {
+      if (type_img != data.img_type) {
+        chrome.storage.local.remove(['expires_date', 'expires_month', 'expires_year'])
+      }
+    })
+    if (document.getElementById('newtab').checked) {
+      var quote_change = 1
+    } else if (document.getElementById('day').checked) {
+      var quote_change = 2
+    }
+
+    var quotes_checked = document.getElementById('quoteSwitch').checked;
+    chrome.storage.sync.set({'sync': document.getElementById('syncCheckInner').checked});
+    chrome.storage.local.set({'img_type': type_img});
+    chrome.storage.local.set({'quotes': quotes_checked});
+    chrome.storage.local.set({'changeQuote': quote_change});
   }
 
-  var quotes_checked = document.getElementById('quoteSwitch').checked;
-  chrome.storage.sync.set({'sync': document.getElementById('syncCheckInner').checked});
-  chrome.storage.local.set({'img_type': type_img});
-  chrome.storage.local.set({'quotes': quotes_checked});
-  chrome.storage.local.set({'changeQuote': quote_change});
-    var alert = document.getElementById('alert');
-    $("#alert").first().hide().slideDown(500).delay(5000).slideUp(500, function () {
-         $(this).hide();
-    });
-}
-function restore_options() {
-  chrome.storage.local.get({
-    img_type: 'nature',
-    quotes: true,
-    changeQuote: 2
-  }, function(items) {
-    document.getElementById('image_type').value = items.img_type;
-    document.getElementById('quoteSwitch').checked = items.quotes;
-    if (items.changeQuote == 1) {
-      document.getElementById('newtab').checked = true;
-      document.getElementById('day').checked = false;
-    } else {
-      document.getElementById('day').checked = true;
-      document.getElementById('newtab').checked = false;
-    }
-    if (items.quotes == false) {
-      document.getElementById('newtab').disabled = true;
-      document.getElementById('day').disabled = true;
-      document.getElementById('disable').classList.remove("text-body");
-      document.getElementById('disable').classList.add("text-muted");
-    }
+  var alert = document.getElementById('alert');
+  $("#alert").first().hide().slideDown(500).delay(5000).slideUp(500, function () {
+       $(this).hide();
   });
+}
+
+function restore_options() {
+  chrome.storage.sync.get(['sync'], function (data) {
+  if (data.sync) {
+    chrome.storage.sync.get({
+      img_type: 'nature',
+      quotes: true,
+      changeQuote: 2
+    }, function(items) {
+      document.getElementById('image_type').value = items.img_type;
+      document.getElementById('quoteSwitch').checked = items.quotes;
+      if (items.changeQuote == 1) {
+        document.getElementById('newtab').checked = true;
+        document.getElementById('day').checked = false;
+      } else {
+        document.getElementById('day').checked = true;
+        document.getElementById('newtab').checked = false;
+      }
+      if (items.quotes == false) {
+        document.getElementById('newtab').disabled = true;
+        document.getElementById('day').disabled = true;
+        document.getElementById('disable').classList.remove("text-body");
+        document.getElementById('disable').classList.add("text-muted");
+      }
+    });
+  } else {
+    chrome.storage.local.get({
+      img_type: 'nature',
+      quotes: true,
+      changeQuote: 2
+    }, function(items) {
+      document.getElementById('image_type').value = items.img_type;
+      document.getElementById('quoteSwitch').checked = items.quotes;
+      if (items.changeQuote == 1) {
+        document.getElementById('newtab').checked = true;
+        document.getElementById('day').checked = false;
+      } else {
+        document.getElementById('day').checked = true;
+        document.getElementById('newtab').checked = false;
+      }
+      if (items.quotes == false) {
+        document.getElementById('newtab').disabled = true;
+        document.getElementById('day').disabled = true;
+        document.getElementById('disable').classList.remove("text-body");
+        document.getElementById('disable').classList.add("text-muted");
+      }
+    });
+  }
+});
   chrome.storage.sync.get({
     sync: true,
   }, function(item) {
     document.getElementById('syncCheckInner').checked = item.sync;
   });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
 document.getElementById('back').addEventListener('click', close_save);
